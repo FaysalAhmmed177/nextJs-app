22475f9d9b439e989491ba2715eca672af2b10d6
Fayal Ahmmed <faysalahmmed177@gmail.com>
2021-11-05 18:45:46 +0600

    connect with mongodb and create a new meetup

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,25 @@
+import { MongoClient } from "mongodb";
+
+// /api/new-meetup
+// POST /api/new-meetup
+
+async function handler(req, res) {
+  if (req.method === "POST") {
+    // Process a POST request
+    const data = req.body;
+    console.log(data);
+    const client = await MongoClient.connect(
+      "mongodb+srv://Faysal177:FaySal177@cluster0.mdqha.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
+  }
+}
+export default handler;
--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -2,8 +2,17 @@ import React from "react";
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
 
 const NewMeetupPage = () => {
-  const onAddMeetup = (enteredMeetupData) => {
+  const onAddMeetup = async (enteredMeetupData) => {
     console.log(enteredMeetupData);
+    const response = await fetch("/api/new-meetup", {
+      method: "POST",
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = response.json();
+    console.log(data.message);
   };
 
   return <NewMeetupForm onAddMeetup={onAddMeetup} />;
